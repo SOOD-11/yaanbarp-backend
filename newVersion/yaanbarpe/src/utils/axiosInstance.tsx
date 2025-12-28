c890de534b3b48d02fed1d59d984de6134fda242
JEFF ronnie <sanchit.mitmpl2023@learner.manipal.edu>
2025-12-29 03:19:41 +0530

    backend link

--- newVersion/yaanbarpe/src/utils/axiosInstance.tsx
+++ newVersion/yaanbarpe/src/utils/axiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL, // Replace with your backend URL
+    baseURL: "https://yaanbarpe-backend.onrender.com/api/v1", // Replace with your backend URL
     headers: {
         'Content-Type': 'application/json',
     },
